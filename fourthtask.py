19114de669e9a10bc99b388d10729311b9725212
Vanshika <90713260+Vanshika3114@users.noreply.github.com>
2022-04-10 15:51:28 +0530

    Add files via upload

---
+++ fourthtask.py
@@ -0,0 +1,17 @@
+# Python Program to Illustrate Different Set Operations.
+
+# define  sets
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+
+# set of  union
+print("Union of E and N is",E | N)
+
+# set of intersection
+print("Intersection of E and N is",E & N)
+
+# set of difference
+print("Difference of E and N is",E - N)
+
+# set of symmetric difference
+print("Symmetric difference of E and N is",E ^ N)
